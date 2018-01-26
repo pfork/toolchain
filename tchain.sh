3ac94e24b17c2f8c692899e1cad9eccade845861
stef <pitchfork@ctrlc.hu>
2018-01-26 15:39:55 +0100

    [enh] bumped versions, added signature verification

--- tchain.sh
+++ tchain.sh
@@ -19,13 +19,13 @@ if [ -z "${CROSS_PREFIX}" ] ; then
     CROSS_PREFIX="$PWD"
 fi
 
-BINUTILS_VERSION="2.27"
-GCC_VERSION="6.2.0"
-GDB_VERSION="7.12"
-NEWLIB_VERSION="2.4.0"
+BINUTILS_VERSION="2.29.1" # "2.28"
+GCC_VERSION="7.3.0" #"6.3.0"
+GDB_VERSION="8.0.1"
+NEWLIB_VERSION="3.0.0" # "2.4.0"
 
 BINUTILS="binutils-${BINUTILS_VERSION}.tar.gz"
-GCC_CORE="gcc-${GCC_VERSION}.tar.bz2"
+GCC_CORE="gcc-${GCC_VERSION}.tar.xz"
 GDB="gdb-${GDB_VERSION}.tar.xz"
 NEWLIB="newlib-${NEWLIB_VERSION}.tar.gz"
 
@@ -50,14 +50,35 @@ if [ ! -f "${BINUTILS}" ]; then
     wget -c "${BINUTILS_SOURCE}${BINUTILS}"
     check_error $? "Error downloading binutils."
 fi
+if [ ! -f "${BINUTILS}.sig" ]; then
+    wget -c "${BINUTILS_SOURCE}${BINUTILS}.sig"
+    check_error $? "Error downloading binutils signature."
+fi
+gpg --verify "${BINUTILS}.sig" "${BINUTILS}"
+check_error $? "Error verifying binutils signature."
+
 if [ ! -f "${GCC_CORE}" ]; then
     wget -c "${GCC_SOURCE}${GCC_CORE}"
     check_error $? "Error downloading GCC Core."
 fi
+if [ ! -f "${GCC_CORE}.sig" ]; then
+    wget -c "${GCC_SOURCE}${GCC_CORE}.sig"
+    check_error $? "Error downloading GCC Core signature."
+fi
+gpg --verify "${GCC_CORE}.sig" "${GCC_CORE}"
+check_error $? "Error verifying gcc signature."
+
 if [ ! -f "${GDB}" ]; then
     wget -c "${GDB_SOURCE}${GDB}"
     check_error $? "Error downloading GDB."
 fi
+if [ ! -f "${GDB}.sig" ]; then
+    wget -c "${GDB_SOURCE}${GDB}.sig"
+    check_error $? "Error downloading GDB signature."
+fi
+gpg --verify "${GDB}.sig" "${GDB}"
+check_error $? "Error verifying gdb signature."
+
 if [ ! -f "${NEWLIB}" ]; then
     wget -c "${NEWLIB_SOURCE}${NEWLIB}"
     check_error $? "Error downloading newlib."
@@ -78,13 +99,13 @@ if [ ! -d "${OBJDIR}" ]; then
 fi
 
 echo ">>> Unpacking tarballs"
-tar -xvzf "${BINUTILS}"
+tar -xvaf "${BINUTILS}"
 check_error $? "Error unpacking binutils."
-tar -xvjf "${GCC_CORE}"
+tar -xvaf "${GCC_CORE}"
 check_error $? "Error unpacking GCC Core."
-tar -xvJf "${GDB}"
+tar -xvaf "${GDB}"
 check_error $? "Error unpacking GDB."
-tar -xvzf "${NEWLIB}"
+tar -xvaf "${NEWLIB}"
 check_error $? "Error unpacking NEWLIB"
 
 echo ">>> Compiling and installing binutils"
@@ -98,8 +119,7 @@ check_error $? "Error compiling/installing binutils."
 echo ">>> Compiling and installing GCC"
 cd "${OBJDIR}"
 check_error $? "Change directory failed."
-#"${GCCDIR}/configure" "--target=${TARGET}" "--prefix=${PREFIX}" "--program-prefix=${TARGET}-" --with-gnu-as --with-gnu-ld --disable-nls --disable-threads --enable-languages=c,c++ --disable-multilib --disable-libgcj --enable-libssp -disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath -disable-libstdcxx-pch --disable-tls --with-newlib --with-python-dir=share/gcc-arm-none-eabi --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --without-headers --with-gxx-include-dir=/usr/include/c++/5.3.0
-"${GCCDIR}/configure" "--target=${TARGET}" "--prefix=${PREFIX}" "--program-prefix=${TARGET}-" --with-cpu=cortex-m3 --with-mode=thumb --with-gnu-as --with-gnu-ld --disable-nls --disable-threads --enable-languages=c,c++ --disable-multilib --disable-libgcj --enable-libssp -disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath -disable-libstdcxx-pch --disable-tls --with-python-dir=share/gcc-arm-none-eabi --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --without-headers --with-gxx-include-dir=/usr/include/c++/5.3.0
+"${GCCDIR}/configure" "--target=${TARGET}" "--prefix=${PREFIX}" "--program-prefix=${TARGET}-" --with-cpu=cortex-m3 --with-mode=thumb --with-gnu-as --with-gnu-ld --disable-nls --disable-threads --enable-languages=c,c++ --disable-multilib --disable-libgcj --enable-libssp -disable-decimal-float --disable-libffi --disable-libgomp --disable-libmudflap --disable-libquadmath -disable-libstdcxx-pch --disable-tls --with-python-dir=share/gcc-arm-none-eabi --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --without-headers --with-gxx-include-dir=/usr/include/c++/6.3.0
 check_error $? "Error configuring GCC."
 PATH="${PATH}:${PREFIX}/bin" make all-gcc install-gcc all-target-libgcc install-target-libgcc
 check_error $? "Error compiling/installing GCC."
@@ -107,7 +127,7 @@ check_error $? "Error compiling/installing GCC."
 echo ">>> Compiling and installing gdb"
 cd "${GDBDIR}"
 check_error $? "Change directory failed."
-./configure "--target=${TARGET}" "--prefix=${PREFIX}" "--program-prefix=${TARGET}-" "--disable-nls"
+./configure --with-python "--target=${TARGET}" "--prefix=${PREFIX}" "--program-prefix=${TARGET}-" "--disable-nls"
 check_error $? "Error configuring gdb."
 make all install
 check_error $? "Error compiling/installing gdb."
